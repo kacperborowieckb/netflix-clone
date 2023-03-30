868a8ac91482669904489e535c2ef367435c7a48
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-03-30 12:55:47 +0200

    added dropdown

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,26 @@
 import '../global-styles/styles.scss';
+import Dropdown from './Dropdown';
+import { useState } from 'react';
 
 const Navbar = () => {
+  const [showDropdown, setShowDropdown] = useState(false);
+  const [checkTimeout, setCheckTimeout] = useState();
+
+  const openDropdown = () => {
+    clearTimeout(checkTimeout);
+    setShowDropdown(true);
+  };
+
+  const closeDropdown = () => {
+    setCheckTimeout(
+      setTimeout(() => {
+        if (showDropdown === true) {
+          setShowDropdown(false);
+        }
+      }, 250)
+    );
+  };
+
   return (
     <div className="navbar">
       <img src="/src/assets/netflix-logo.png" alt="netflix logo" className="navbar__logo" />
@@ -25,24 +45,21 @@ const Navbar = () => {
         </li>
       </ul>
       <div className="navbar__user-section">
-        <img
-          className="navbar__search-icon"
-          src="/src/assets/search-icon.svg"
-          alt=""
-          width={'32px'}
-        />
+        <img className="navbar__search-icon" src="/src/assets/search-icon.svg" alt="" />
         <img
           className="navbar__notifications-icon"
           src="/src/assets/notification-icon.svg"
-          width={'32px'}
           alt=""
         />
-        <img
-          className="navbar__profile-picture"
-          src="/src/assets/profile-picture.jpg"
-          alt="profile picture"
-          width={'48px'}
-        />
+        <div className="navbar__profile" onMouseOver={openDropdown} onMouseLeave={closeDropdown}>
+          <img
+            className="navbar__profile-picture"
+            src="/src/assets/profile-picture.jpg"
+            alt="profile picture"
+          />
+          <img src="/src/assets/dropdown-arrow.svg" alt="" className="navbar__dropdown-arrow" />
+          {showDropdown ? <Dropdown /> : <></>}
+        </div>
       </div>
     </div>
   );

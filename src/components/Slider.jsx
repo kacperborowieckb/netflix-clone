723d81ec9b0d273d353f4481f089abf4a88ea448
kacperborowieckb <kacperborowiec.kb@gmail.com>
2023-03-31 10:48:33 +0200

    slide animation

--- src/components/Slider.jsx
+++ src/components/Slider.jsx
@@ -1,11 +1,24 @@
 import '../global-styles/styles.scss';
 
 const Slider = ({ title }) => {
+  const slideAnimate = (direction) => {
+    const slider = document.querySelector('.slider__items-container');
+    let index = getComputedStyle(slider).getPropertyValue('--index');
+    index = direction === 'left' ? Number(index) - 1 : Number(index) + 1;
+    slider.style.transform = `translateX(calc(-100% * ${index})`;
+    slider.style.setProperty('--index', index);
+  };
+
   return (
     <div className="slider">
       <h3 className="slider__heading">{title}</h3>
       <div className="slider__container">
-        <button className="slider__left">
+        <button
+          className="slider__left"
+          onClick={() => {
+            slideAnimate('left');
+          }}
+        >
           <img src="/src/assets/previous.svg" alt="" />
         </button>
         <div className="slider__items-container">
@@ -22,7 +35,12 @@ const Slider = ({ title }) => {
           })}
         </div>
 
-        <button className="slider__right">
+        <button
+          className="slider__right"
+          onClick={() => {
+            slideAnimate('right');
+          }}
+        >
           <img src="/src/assets/next.svg" alt="" />
         </button>
       </div>
